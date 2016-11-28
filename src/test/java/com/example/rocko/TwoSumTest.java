797d9445d5ce4ebf09dd10982ac61ba977f1d40d
Rocko UrAD <roc26002w@gmail.com>
2016-11-28 18:43:24 +0800

    新增 | 在測試上新增題目註解 | Rocko

--- src/test/java/com/example/rocko/TwoSumTest.java
+++ src/test/java/com/example/rocko/TwoSumTest.java
@@ -9,6 +9,12 @@ import java.util.Arrays;
 
 /**
  * Created by Rocko on 2016/11/28.
+ * 給予一組整數陣列及一個數字，這個數字將是陣列中隨便二個數的相加，並回傳此二數的key 值
+ * 必需要符合下面的範例
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
  */
 @Slf4j
 public class TwoSumTest {
