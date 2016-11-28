b4999e3df8cd85da6740b92a3dc3135dadf1e6be
Rocko UrAD <roc26002w@gmail.com>
2016-11-28 18:37:36 +0800

    加入 | 加入TwoSum Class & 更新Test 檔案| Rocko

---
+++ src/main/java/com/example/rocko/TwoSum.java
@@ -0,0 +1,13 @@
+package com.example.rocko;
+
+/**
+ * Created by Rocko on 2016/11/28.
+ */
+public class TwoSum {
+
+
+    public int[] getSumTwo(int[] list, Integer target) {
+        return new int[0];
+    }
+
+}
--- src/test/java/com/example/rocko/TwoSumTest.java
+++ src/test/java/com/example/rocko/TwoSumTest.java
@@ -3,6 +3,7 @@ package com.example.rocko;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.Arrays;
 
@@ -19,13 +20,14 @@ public class TwoSumTest {
         twoSum = new TwoSum();
     }
 
+    @Test
     public void testGetTarget9() {
         int[] list = new int[]{2, 7, 11, 15};
         Integer target = 9;
 
         int[] act = twoSum.getSumTwo(list, target);
 
-        Assert.assertEquals("[7, 0, 7]", Arrays.toString(act));
+        Assert.assertEquals("[0, 1]", Arrays.toString(act));
     }
 
 }
