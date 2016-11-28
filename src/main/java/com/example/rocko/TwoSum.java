97d24536d599469c5d4d0b71a3f2552e917db656
Rocko UrAD <roc26002w@gmail.com>
2016-11-28 18:40:04 +0800

    完成| 完成TwoSum的題目 | Rocko

--- src/main/java/com/example/rocko/TwoSum.java
+++ src/main/java/com/example/rocko/TwoSum.java
@@ -1,5 +1,9 @@
 package com.example.rocko;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Rocko on 2016/11/28.
  */
@@ -7,7 +11,18 @@ public class TwoSum {
 
 
     public int[] getSumTwo(int[] list, Integer target) {
-        return new int[0];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < list.length; i++) {
+            int complate = target - list[i];
+            if (map.containsKey(complate)) {
+                return new int[]{map.get(complate), i};
+            }
+
+            map.put(list[i], i);
+        }
+
+        return new int[]{-1 , -1};
     }
 
 }
