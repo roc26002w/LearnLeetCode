3d4333de383a4ecbd052d4bebcdccc25db4aae6c
Rocko UrAD <roc26002w@gmail.com>
2016-12-13 16:11:52 +0800

    [Problem. 226] Add | Add Other Test Case | Rocko

--- src/InvertBinaryTree.php
+++ src/InvertBinaryTree.php
@@ -11,7 +11,24 @@ class InvertBinaryTree
     {
     }
     
-    public function invertTree($array)
+    public function invertTree(Array $originTree): Array
     {
+        
+        $tmpTree = [];
+        foreach ($originTree as $key => $item) {
+            if ($key > 0) {
+                if ($key % 2 == 1) {
+                    array_push($tmpTree, $item);
+                    if(!is_null($originTree[$key + 1])){
+                        $originTree[$key] = $originTree[$key + 1];
+                    }
+                } else {
+                    $originTree[$key] = array_pop($tmpTree);
+                }
+            }
+        }
+        
+        return $originTree;
+        
     }
 }
--- tests/src/InvertBinaryTreeTest.php
+++ tests/src/InvertBinaryTreeTest.php
@@ -26,9 +26,40 @@ class InvertBinaryTreeTest extends TestCase
         //Arrange
         
         //Act
-        $actual = $this->invertBinaryTree->invertTree([2, 3, 4]);
+        $actual = $this->invertBinaryTree->invertTree([2, 4, 3]);
         
         //Assert
         $this->assertEquals([2, 3, 4], $actual);
+    }
+    
+    /**
+     * @group InvertBinaryTreeTest
+     * @test
+     */
+    public function given_3_5_1_should_return_3_1_5()
+    {
+        //Arrange
+        
+        //Act
+        $actual = $this->invertBinaryTree->invertTree([3, 5, 1]);
+        
+        //Assert
+        $this->assertEquals([3, 1, 5], $actual);
+    }
+    
+    
+    /**
+     * @group InvertBinaryTreeTest
+     * @test
+     */
+    public function given_4_2_7_1_3_6_9_should_return_4_7_2_9_6_3_1()
+    {
+        //Arrange
+        
+        //Act
+        $actual = $this->invertBinaryTree->invertTree([4, 2, 7, 1, 3, 6, 9]);
+        
+        //Assert
+        $this->assertEquals([4, 7, 2, 9, 6, 3, 1], $actual);
     }
 }
