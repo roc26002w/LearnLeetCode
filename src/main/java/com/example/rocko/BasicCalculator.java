5d0e1b0636672ec6f50d561118f2a3f86d8684e1
Rocko UrAD <roc26002w@gmail.com>
2016-12-14 18:01:21 +0800

    更新 | 完成問題 224 Test Case 1 | Rocko

--- src/main/java/com/example/rocko/BasicCalculator.java
+++ src/main/java/com/example/rocko/BasicCalculator.java
@@ -4,4 +4,21 @@ package com.example.rocko;
  * Created by Rocko on 2016/12/14.
  */
 public class BasicCalculator {
+
+    public int calculate(String s) {
+
+        int result = 0;
+
+        for (int i = 0 ; i < s.length() ; i ++){
+
+            if(Character.isDigit(s.charAt(i))){
+                System.out.println(s.charAt(i));
+                result += Character.getNumericValue(s.charAt(i));
+            }
+
+        }
+        System.out.println(result);
+
+        return result;
+    }
 }
