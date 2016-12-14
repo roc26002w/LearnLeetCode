d39b2fb43839d65c611a06e61df843d4e3352822
Rocko UrAD <roc26002w@gmail.com>
2016-12-14 18:02:38 +0800

    更新 | 完成問題 224 Test Case 2 | Rocko

--- src/main/java/com/example/rocko/BasicCalculator.java
+++ src/main/java/com/example/rocko/BasicCalculator.java
@@ -9,11 +9,18 @@ public class BasicCalculator {
 
         int result = 0;
 
+        int sign = 1;
+
         for (int i = 0 ; i < s.length() ; i ++){
 
             if(Character.isDigit(s.charAt(i))){
                 System.out.println(s.charAt(i));
-                result += Character.getNumericValue(s.charAt(i));
+                result += sign * Character.getNumericValue(s.charAt(i));
+            } else if(String.valueOf(s.charAt(i)).equals("+")){
+                sign = 1;
+
+            } else if(String.valueOf(s.charAt(i)).equals("-")){
+                sign = -1;
             }
 
         }
