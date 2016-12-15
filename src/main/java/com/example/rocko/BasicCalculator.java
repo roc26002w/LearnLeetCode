ad3e680ea66162f70a5bc609a273c9701d13f1d0
Rocko UrAD <roc26002w@gmail.com>
2016-12-15 17:48:00 +0800

    更新 | 完成問題 224 Test Case 5 | Rocko

--- src/main/java/com/example/rocko/BasicCalculator.java
+++ src/main/java/com/example/rocko/BasicCalculator.java
@@ -13,41 +13,58 @@ public class BasicCalculator {
 
         int sign = 1;
 
+        int num = 0;
+
         boolean flag = true;
 
-        String tmp="";
+        String tmp = "";
 
         Stack<Integer> stack = new Stack();
 
         for (int i = 0; i < s.length(); i++) {
 
-            if (String.valueOf(s.charAt(i)).equals("(")) {
+            Character character = s.charAt(i);
+
+            if (Character.isDigit(character)) {
+
+                if (i == 0) {
+                    result = Character.getNumericValue(s.charAt(0));
+                }
+
+                if (i - 1 >= 0 && !Character.isDigit(s.charAt(i - 1))) {
+
+                    while ((i + num) < s.length() && Character.isDigit(s.charAt(i + num))) {
+                        num++;
+                    }
+
+                    result += sign * Integer.parseInt(s.substring(i, i + num));
+                    num = 0;
+                }
+
+                tmp += character;
+            } else if (String.valueOf(character).equals("(")) {
                 stack.push(result);
                 stack.push(sign);
                 sign = 1;
                 result = 0;
                 flag = false;
 
-            } else if (String.valueOf(s.charAt(i)).equals(")")) {
+            } else if (String.valueOf(character).equals(")")) {
                 result = stack.pop() * result + stack.pop();
                 flag = false;
 
-            } else if (String.valueOf(s.charAt(i)).equals("+")) {
+            } else if (String.valueOf(character).equals("+")) {
                 sign = 1;
                 flag = false;
 
-            } else if (String.valueOf(s.charAt(i)).equals("-")) {
+            } else if (String.valueOf(character).equals("-")) {
                 sign = -1;
                 flag = false;
-
-            } else if (Character.isDigit(s.charAt(i))) {
-
-                tmp += s.charAt(i);
-                result += sign * Character.getNumericValue(s.charAt(i));
 
             }
 
         }
+
 
         if (flag == true) {
             result = Integer.valueOf(tmp).intValue();
