a2a17965d2cfe722fefcce25d43122ea91e4295a
Rocko UrAD <roc26002w@gmail.com>
2016-12-16 10:30:57 +0800

    重構 | 重構BasicCalculator 效能更好 | Rocko

--- src/main/java/com/example/rocko/BasicCalculator.java
+++ src/main/java/com/example/rocko/BasicCalculator.java
@@ -10,64 +10,29 @@ public class BasicCalculator {
     public int calculate(String s) {
 
         int result = 0;
-
         int sign = 1;
-
-        int num = 0;
-
-        boolean flag = true;
-
-        String tmp = "";
-
-        Stack<Integer> stack = new Stack();
+        Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
-
-            Character character = s.charAt(i);
-
-            if (Character.isDigit(character)) {
-
-                if (i == 0) {
-                    result = Character.getNumericValue(s.charAt(0));
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                int current = ch - '0';
+                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    current = current * 10 + s.charAt(++i) - '0';
                 }
-
-                if (i - 1 >= 0 && !Character.isDigit(s.charAt(i - 1))) {
-
-                    while ((i + num) < s.length() && Character.isDigit(s.charAt(i + num))) {
-                        num++;
-                    }
-
-                    result += sign * Integer.parseInt(s.substring(i, i + num));
-                    num = 0;
-                }
-
-                tmp += character;
-            } else if (String.valueOf(character).equals("(")) {
+                result = result + sign * current;
+            } else if (ch == '+') {
+                sign = 1;
+            } else if (ch == '-') {
+                sign = -1;
+            } else if (ch == '(') {
                 stack.push(result);
                 stack.push(sign);
-                sign = 1;
                 result = 0;
-                flag = false;
-
-            } else if (String.valueOf(character).equals(")")) {
-                result = stack.pop() * result + stack.pop();
-                flag = false;
-
-            } else if (String.valueOf(character).equals("+")) {
                 sign = 1;
-                flag = false;
-
-            } else if (String.valueOf(character).equals("-")) {
-                sign = -1;
-                flag = false;
-
+            } else if (ch == ')') {
+                result = stack.pop() * result + stack.pop();
             }
-
-        }
-
-
-        if (flag == true) {
-            result = Integer.valueOf(tmp).intValue();
         }
 
         return result;
